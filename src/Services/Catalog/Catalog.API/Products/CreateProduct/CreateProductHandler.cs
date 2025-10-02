d3dc0221729f89776155ac0b801d1739d186d5b2
Jimmy Grados <jgrados@osiptel.gob.pe>
2025-10-02 14:17:07 -0500

    [20251002] Docker Compose File

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,11 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler (IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -23,11 +20,13 @@ namespace Catalog.API.Products.CreateProduct
                 Price = command.Price,
             };
 
-            // TODO
             // save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
 
-            // return CreateProducResult result
-            return new CreateProductResult(Guid.NewGuid());
+            // return result
+            /// return new CreateProductResult(Guid.NewGuid());
+            return new CreateProductResult(product.Id);
         }
     }
 }
