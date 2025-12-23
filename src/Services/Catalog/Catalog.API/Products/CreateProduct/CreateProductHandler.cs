ffdabd48e71dc270dba46d649d9119774366044c
Jimmy Grados <jgrados@osiptel.gob.pe>
2025-12-23 18:20:04 -0500

    Actualizacion

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,32 +1,32 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler (IDocumentSession session) 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-        : ICommand<CreateProductResult>;
-
-    public record CreateProductResult(Guid Id);
-
-    internal class CreateProductCommandHandler (IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        // create Product entity from command object
+
+        var product = new Product
         {
-            // create Product entity from command object
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
 
-            var product = new Product
-            {
-                Name = command.Name,
-                Category = command.Category,
-                Description = command.Description,
-                ImageFile = command.ImageFile,
-                Price = command.Price,
-            };
+        // save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
-            // save to database
-            session.Store(product);
-            await session.SaveChangesAsync(cancellationToken);
-
-            // return result
-            /// return new CreateProductResult(Guid.NewGuid());
-            return new CreateProductResult(product.Id);
-        }
+        // return result
+        /// return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }
