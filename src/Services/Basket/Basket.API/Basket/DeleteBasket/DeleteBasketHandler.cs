2590bf949638f2bd45b52221dceefb31b6e0786d
Jimmy Grados <jgrados@osiptel.gob.pe>
2026-01-06 18:59:36 -0500

    Actualizacion

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleBasketCommand>
+{
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class DeleteBasketCommandHandler
+    : ICommandHandler<DeleBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleBasketCommand command, CancellationToken cancellationToken)
+    {
+        //TODO: delete basket from database and cache
+        //session.Delete<Produc>(command.Id);
+
+        return new DeleteBasketResult(true);
+    }
+}
