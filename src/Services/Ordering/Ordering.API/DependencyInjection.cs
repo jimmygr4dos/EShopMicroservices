d88d53d0d24b593f9785ba9361cef69841a46bb0
Jimmy Grados <jgrados@osiptel.gob.pe>
2026-02-02 16:58:24 -0500

    Actualizacion

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        // Add API services here, e.g. Controllers, Swagger, etc.
+        //services.AddCarter();
+
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline here, e.g. Swagger, etc.
+        //app.MapCarter();
+
+        return app;
+    }
+}
